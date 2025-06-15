0c0dbef57e1f3978647db3e6ca8c5b8357acab15
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2025-06-15 18:29:03 +0900

    fix: separate aws mfa setting

--- home/dot_aws/aws-vault-wrapper.sh
+++ home/dot_aws/aws-vault-wrapper.sh
@@ -2,7 +2,6 @@
 
 PROFILE="nagase"
 SESSION_DURATION="12h"
-OP_AWS_ITEM_ID="tb4wzijx2dxtk2g465gunpe7pe"
 
 # セッションの状態を確認する関数
 check_session() {
@@ -21,8 +20,8 @@ check_session() {
 
 # メイン処理
 if check_session; then
-    aws-vault exec $PROFILE --json --prompt=terminal --duration=$SESSION_DURATION
+    aws-vault export $PROFILE --format=json --duration=$SESSION_DURATION
 else
-    mfa_token=$(op item get $OP_AWS_ITEM_ID --otp)
-    aws-vault exec $PROFILE --json --prompt=terminal --duration=$SESSION_DURATION --mfa-token=$mfa_token
+    mfa_token=$(op item get AWS --otp)
+    aws-vault export $PROFILE --format=json --duration=$SESSION_DURATION --mfa-token=$mfa_token
 fi
