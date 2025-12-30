47ae641697c1c28c27c16940ad9fde848881eab8
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2025-12-30 17:24:28 +0900

    feat: add support for mac pinentry

--- nix/modules/home/programs/gpg.nix
+++ nix/modules/home/programs/gpg.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  pinentryPackage = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-curses;
+in
 {
   programs.gpg = {
     enable = true;
@@ -8,8 +11,10 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = false;
-    pinentry.package = pkgs.pinentry-curses;
+    pinentry.package = pinentryPackage;
     defaultCacheTtl = 43200;
     maxCacheTtl = 43200;
   };
+
+  home.packages = [ pinentryPackage ];
 }
