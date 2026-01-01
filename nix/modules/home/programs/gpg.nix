8f6c997085272ee07c8077c2ad261dd1ac5e321a
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2026-01-01 22:43:13 +0900

    feat: add pinentry script for 1password

--- nix/modules/home/programs/gpg.nix
+++ nix/modules/home/programs/gpg.nix
@@ -1,6 +1,32 @@
 { pkgs, lib, ... }:
 let
-  pinentryPackage = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-curses;
+  # https://gist.github.com/mrgrain/9c3519952d9af811bd7bf50bfcfaa16f
+  pinentry-1password = pkgs.writeShellScriptBin "pinentry-1password" ''
+    if grep -qi microsoft /proc/version 2>/dev/null; then
+      OP_CMD="/mnt/c/Users/zhouc/scoop/shims/op.exe"
+    else
+      OP_CMD="op"
+    fi
+
+    echo "OK"
+    while IFS= read -r line; do
+      cmd=$(echo "$line" | cut -d' ' -f1)
+      case "$cmd" in
+        GETPIN)
+          PASSPHRASE=$($OP_CMD read "op://Personal/GPG Key/password" --account my.1password.com 2>/dev/null)
+          echo "D $PASSPHRASE"
+          echo "OK"
+          ;;
+        BYE)
+          echo "OK"
+          exit 0
+          ;;
+        *)
+          echo "OK"
+          ;;
+      esac
+    done
+  '';
 in
 {
   programs.gpg = {
@@ -11,10 +37,11 @@ in
   services.gpg-agent = {
     enable = true;
     enableSshSupport = false;
-    pinentry.package = pinentryPackage;
+    pinentry = {
+      package = pinentry-1password;
+      program = "pinentry-1password";
+    };
     defaultCacheTtl = 43200;
     maxCacheTtl = 43200;
   };
-
-  home.packages = [ pinentryPackage ];
 }
