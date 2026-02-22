0aef0e81985e4c54dc1cea33ddda2f18f5be8bf8
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2026-02-22 16:40:33 +0900

    feat: add gcloud detect_env_vars to starship config
    Show gcloud module in prompt when CLOUDSDK_ACTIVE_CONFIG_NAME
    environment variable is set, indicating an active gcloud profile.

--- nix/modules/home/programs/starship.nix
+++ nix/modules/home/programs/starship.nix
@@ -3,6 +3,9 @@
   programs.starship = {
     enable = true;
     settings = {
+      gcloud = {
+        detect_env_vars = [ "CLOUDSDK_ACTIVE_CONFIG_NAME" ];
+      };
       python = {
         detect_extensions = [ ];
         detect_files = [ ];
