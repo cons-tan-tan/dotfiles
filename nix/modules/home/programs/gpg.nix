3a3b2ea836642d20bca1239bb1458c4506cae6f4
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2026-04-01 12:07:12 +0900

    refactor: replace pinentry-1password with Windows pinentry.exe
    Remove the custom pinentry-1password shell script that fetched
    GPG passphrases from 1Password CLI. Instead, delegate pinentry
    to Windows-side gpg4win's pinentry.exe via WSL path, simplifying
    the setup and removing the 1Password CLI dependency for GPG.

--- nix/modules/home/programs/gpg.nix
+++ nix/modules/home/programs/gpg.nix
@@ -2,33 +2,6 @@
 let
   isDarwin = pkgs.stdenv.isDarwin;
 
-  # https://gist.github.com/mrgrain/9c3519952d9af811bd7bf50bfcfaa16f
-  pinentry-1password = pkgs.writeShellScriptBin "pinentry-1password" ''
-    if grep -qi microsoft /proc/version 2>/dev/null; then
-      OP_CMD="/mnt/c/Users/zhouc/scoop/shims/op.exe"
-    else
-      OP_CMD="op"
-    fi
-
-    echo "OK"
-    while IFS= read -r line; do
-      cmd=$(echo "$line" | cut -d' ' -f1)
-      case "$cmd" in
-        GETPIN)
-          PASSPHRASE=$($OP_CMD read "op://Personal/GPG Key/password" --account my.1password.com 2>/dev/null)
-          echo "D $PASSPHRASE"
-          echo "OK"
-          ;;
-        BYE)
-          echo "OK"
-          exit 0
-          ;;
-        *)
-          echo "OK"
-          ;;
-      esac
-    done
-  '';
 in
 {
   programs.gpg = {
@@ -43,10 +16,10 @@ in
       if isDarwin then
         { package = pkgs.pinentry_mac; }
       else
-        {
-          package = pinentry-1password;
-          program = "pinentry-1password";
-        };
+        { package = null; };
+    extraConfig = lib.optionalString (!isDarwin) ''
+      pinentry-program /mnt/c/Users/zhouc/scoop/apps/gpg4win/current/Gpg4win/bin/pinentry.exe
+    '';
     defaultCacheTtl = 43200;
     maxCacheTtl = 43200;
     sshKeys = [
