3d12df3f4e3615595c30dbfd008531abd2045fe3
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2026-06-05 23:46:27 +0900

    fix: import gpg ssh socket directly on WSL

--- nix/modules/home/programs/gpg.nix
+++ nix/modules/home/programs/gpg.nix
@@ -52,6 +52,17 @@ let
   '';
 
   windowsSshcontrol = pkgs.writeText "windows-sshcontrol" (lib.concatStringsSep "\n" sshKeygrips);
+
+  wslSetSshAuthSock = pkgs.writeShellScript "set-SSH_AUTH_SOCK-wsl" ''
+    if [ -z "''${SSH_AUTH_SOCK:-}" ] || [ -z "''${SSH_CONNECTION:-}" ]; then
+      unset SSH_AGENT_PID
+      if [ "''${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+        export SSH_AUTH_SOCK="$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)"
+      fi
+    fi
+
+    ${pkgs.systemd}/bin/systemctl --user import-environment SSH_AUTH_SOCK
+  '';
 in
 {
   programs.gpg = {
@@ -70,6 +81,10 @@ in
     maxCacheTtl = cacheTtl;
     sshKeys = sshKeygrips;
   };
+
+  systemd.user.services.set-SSH_AUTH_SOCK.Service.ExecStart = lib.mkIf hk.isWsl (
+    lib.mkForce wslSetSshAuthSock
+  );
 
   home.activation = lib.mkIf hk.hasWindowsCompanion {
     deployWindowsGpg = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
